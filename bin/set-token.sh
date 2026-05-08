3f6b7d1d2fb16bad6f162fc288857a8ee25bab38
Michael Willer <michael.willer@enterprisedb.com>
2026-05-08 15:21:53 +0200

    .bashrc not .bash_profile

--- bin/set-token.sh
+++ bin/set-token.sh
@@ -4,12 +4,12 @@ then
   echo "Usage: $0 <token>"
   exit 1
 fi
-if grep -q "EDB_SUBSCRIPTION_TOKEN" .bash_profile;
+if grep -q "EDB_SUBSCRIPTION_TOKEN" .bashrc;
 then
   echo "EDB_SUBSCRIPTION_TOKEN already set, skipping"
   exit 0
 fi
 echo "Setting EDB_SUBSCRIPION_TOKEN"
-echo "export EDB_SUBSCRIPION_TOKEN=$1" >> .bash_profile
-echo "export EDB_SUBSCRIPTION_PLAN=enterprise" >> .bash_profile
-echo "export EDB_REPO_TYPE=deb" >> .bash_profile
+echo "export EDB_SUBSCRIPION_TOKEN=$1" >> .bashrc
+echo "export EDB_SUBSCRIPTION_PLAN=enterprise" >> .bashrc
+echo "export EDB_REPO_TYPE=deb" >> .bashrc
