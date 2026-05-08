0df27f4334b37177a9c483808654290035ef547a
Michael Willer <michael.willer@enterprisedb.com>
2026-05-08 15:30:58 +0200

    attempt for set-token

--- bin/set-token.sh
+++ bin/set-token.sh
@@ -4,12 +4,13 @@ then
   echo "Usage: $0 <token>"
   exit 1
 fi
-if grep -q "EDB_SUBSCRIPTION_TOKEN" .bashrc;
+if grep -q "edb-env" .bashrc;
 then
   echo "EDB_SUBSCRIPTION_TOKEN already set, skipping"
-  exit 0
+else
+  echo "source ~./edb-env" >> ~/.bashrc
 fi
 echo "Setting EDB_SUBSCRIPION_TOKEN"
-echo "export EDB_SUBSCRIPION_TOKEN=$1" >> .bashrc
-echo "export EDB_SUBSCRIPTION_PLAN=enterprise" >> .bashrc
-echo "export EDB_REPO_TYPE=deb" >> .bashrc
+echo "export EDB_SUBSCRIPION_TOKEN=$1" > .edb-env
+echo "export EDB_SUBSCRIPTION_PLAN=enterprise" >> .edb-env
+echo "export EDB_REPO_TYPE=deb" >> .edb-env
