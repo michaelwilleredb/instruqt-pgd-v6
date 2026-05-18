573320ef3d3619c80cb545806d25f3f62be155e1
Michael Willer <michael.willer@enterprisedb.com>
2026-05-18 15:23:06 +0200

    pgd configuration (first attempt) - rename script for clarity

---
+++ bin/pdg-config.sh
@@ -0,0 +1,29 @@
+sudo  -iu postgres
+export PG_VERSION=17
+export PATH=$PATH:/usr/edb/pge$PG_VERSION/bin/
+export PGDATA=/var/lib/edb-pge/$PG_VERSION/data/
+export PGPASSWORD=secret
+
+export user=enterprisedb
+export port=5432
+export dbname=pgddb
+
+db1_dsn="host=db-1 user=$user port=$port dbname=$dbname"
+db2_dsn="host=db-2 user=$user port=$port dbname=$dbname"
+db3_dsn="host=db-3 user=$user port=$port dbname=$dbname"
+
+case $1 in
+  db-1)
+    pgd node node-1 setup --dsn $db1_dsn  --group-name group-1
+    ;;
+  db-2)
+    pgd node node-2 setup --dsn $db2_dsn  --group-name group-1 --cluster-dsn $db1_dsn
+    ;;
+  db-3)
+    pgd node node-3 setup --dsn $db3_dsn  --group-name group-1 --cluster-dsn $db1_dsn
+    ;;
+  *)
+    echo "ERROR: UNKNOWN host"
+    exit 0
+    ;;
+esac
--- challenges/1-Configure-PGD.md
+++ challenges/1-Configure-PGD.md
@@ -1,8 +1,23 @@
 
 At this point we have the three servers db-1, db-2 and db-3 provisioned and running. 
 
-Next step is to install EDB Advanced Server and PGD on all three servers.
+---
 
+NOTE: This demo is NOT a workshop. It is not supposed 
+to be shared with participants - unless a temporary EDB Subscription Token is used
+
+---
+
+### Set EDB Suscription token on all servers
 ```bash
 set-env.sh "---YOUR TOKEN---"
 ```
+
+### Install EPAS and PGD
+Next step is to install EDB Advanced Server and PGD on all three servers.
+
+```bash
+run-on-all-dbs.sh "$HOME/instruct-pgd-v6/bin/pgd-install.sh"
+```
+
+
