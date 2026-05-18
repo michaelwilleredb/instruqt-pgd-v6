61ce628fd118ab2d9e34e39a30bc22a753ede420
Michael Willer <michael.willer@enterprisedb.com>
2026-05-18 13:14:38 +0200

    Trying again

--- bin/install-pgd.sh
+++ bin/install-pgd.sh
@@ -2,17 +2,16 @@
 T=/tmp/instruqt-setup.log
 
 if [ -z $EDB_SUBSCRIPTION_TOKEN ]; then
-  echo "Subscription token not set - chickening out of script" >> $T
+  echo "Subscription token not set - chickening out of script"
   exit 0
 fi
 # For PGD Expanded, there are two repositories to install.
-echo "Adding repositories" >> $T
-echo "EDB_SUBSCRIPTION_TOKEN = $EDB_SUBSCRIPTION_TOKEN / $EDB_SUBSCRIPTION_PLAN" >> $T
-curl -1sSLf " https://downloads.enterprisedb.com/$EDB_SUBSCRIPTION_TOKEN/$EDB_SUBSCRIPTION_PLAN/setup.$EDB_REPO_TYPE.sh" | sudo -E bash
-curl -1sSLf " https://downloads.enterprisedb.com/$EDB_SUBSCRIPTION_TOKEN/postgres_distributed/setup.$EDB_REPO_TYPE.sh" | sudo -E bash
+echo "Adding repositories"
+curl -1sSLf "https://downloads.enterprisedb.com/$EDB_SUBSCRIPTION_TOKEN/$EDB_SUBSCRIPTION_PLAN/setup.$EDB_REPO_TYPE.sh" | sudo -E bash
+curl -1sSLf "https://downloads.enterprisedb.com/$EDB_SUBSCRIPTION_TOKEN/postgres_distributed/setup.$EDB_REPO_TYPE.sh" | sudo -E bash
 
 
 export PG_VERSION=17
 export EDB_PACKAGES="edb-as$PG_VERSION-server edb-pgd6-expanded-epas$PG_VERSION"
-echo "Install $EDB_PACKAGES" >> $T
-sudo apt install -y $EDB_PACKAGES && echo "Install completed successfully" >> $T 
+echo "Install $EDB_PACKAGES"
+sudo apt install -y $EDB_PACKAGES 
--- challenges/1-Configure-PGD.md
+++ challenges/1-Configure-PGD.md
@@ -3,3 +3,6 @@ At this point we have the three servers db-1, db-2 and db-3 provisioned and runn
 
 Next step is to install EDB Advanced Server and PGD on all three servers.
 
+```bash
+set-env.sh "---YOUR TOKEN---"
+```
