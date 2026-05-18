292ed1826319dab4eedf940d2da4290260bf2c3f
Michael Willer <michael.willer@enterprisedb.com>
2026-05-18 12:56:49 +0200

    .

--- bin/install-pgd.sh
+++ bin/install-pgd.sh
@@ -1,2 +1,13 @@
-curl -1sSLf "https://downloads.enterprisedb.com/$EDB_SUBSCRIPTION_TOKEN/$EDB_SUBSCRIPTION_PLAN/setup.$EDB_REPO_TYPE.sh" | sudo -E bash
-curl -1sSLf "https://downloads.enterprisedb.com/$EDB_SUBSCRIPTION_TOKEN/postgres_distributed/setup.$EDB_REPO_TYPE.sh" | sudo -E bash
+
+if [ -z $EDB_SUBSCRIPTION_TOKEN ]; then
+  echo "Subscription token not set - chickening out of script"
+  exit 0
+fi
+# For PGD Expanded, there are two repositories to install.
+curl -1sSLf " https://downloads.enterprisedb.com/$EDB_SUBSCRIPTION_TOKEN/$EDB_SUBSCRIPTION_PLAN/setup.$EDB_REPO_TYPE.sh" | sudo -E bash
+curl -1sSLf " https://downloads.enterprisedb.com/$EDB_SUBSCRIPTION_TOKEN/postgres_distributed/setup.$EDB_REPO_TYPE.sh" | sudo -E bash
+
+
+export PG_VERSION=17
+export EDB_PACKAGES="edb-as$PG_VERSION-server edb-pgd6-<edition>-epas$PG_VERSION"
+sudo apt install -y $EDB_PACKAGES
