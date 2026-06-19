95936272d06609aa7a68b78dfd33c21c5a24f4d3
Michael Willer <michael.willer@enterprisedb.com>
2026-06-19 12:04:22 +0200

    adjusting paths

--- bin/pgd-config.sh
+++ bin/pgd-config.sh
@@ -1,26 +1,31 @@
 sudo  -iu enterprisedb bash << 'EOF'
 export PG_VERSION=17
-export PATH=$PATH:/usr/edb/pge$PG_VERSION/bin/
-export PGDATA=/var/lib/edb-pge/$PG_VERSION/data/
+export PATH=$PATH:/usr/edb/as-common/bin
+export PGDATA=/var/lib/edb-as/$PG_VERSION/data/
 export PGPASSWORD=secret
 
 export user=enterprisedb
-export port=5432
-export dbname=pgddb
+export port=5444
+export dbname=edb # edb database already exists, we'll reuse it.
 
 db1_dsn="host=db-1 user=$user port=$port dbname=$dbname"
 db2_dsn="host=db-2 user=$user port=$port dbname=$dbname"
 db3_dsn="host=db-3 user=$user port=$port dbname=$dbname"
 
+# Link all config-files (workaround for the pgd CLI)
+find /etc/edb-as/17/main -maxdepth 1 -type f -exec ln -sf {} /var/lib/edb-as/17/main/ \;
+
+extra_options="--bindir /usr/lib/edb-as/17/bin"
+
 case $1 in
   db-1)
-    pgd node node-1 setup --dsn "$db1_dsn"  --group-name group-1
+    pgd node node-1 setup --dsn "$db1_dsn"  --group-name group-1 $extra_options
     ;;
   db-2)
-    pgd node node-2 setup --dsn "$db2_dsn"  --group-name group-1 --cluster-dsn "$db1_dsn"
+    pgd node node-2 setup --dsn "$db2_dsn"  --group-name group-1 --cluster-dsn "$db1_dsn" $extra_options
     ;;
   db-3)
-    pgd node node-3 setup --dsn "$db3_dsn"  --group-name group-1 --cluster-dsn "$db1_dsn"
+    pgd node node-3 setup --dsn "$db3_dsn"  --group-name group-1 --cluster-dsn "$db1_dsn" $extra_options
     ;;
   *)
     echo "ERROR: UNKNOWN host"
