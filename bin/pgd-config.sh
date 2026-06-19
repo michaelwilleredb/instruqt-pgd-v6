f097f8d8b9ff2b994ce22e1b552f73703c691ab6
Michael Willer <michael.willer@enterprisedb.com>
2026-06-19 16:18:40 +0200

    .

--- bin/pgd-config.sh
+++ bin/pgd-config.sh
@@ -1,6 +1,10 @@
 #
 # pgd_config.sh
 #
+restart_db(){
+  systemctl restart pgd # Ensure clean start
+  until $PG_BINDIR/pg_isready -h localhost -p $dbport; do sleep 1; done
+}
 create_service(){
   cat > /etc/systemd/system/pgd.service << SVC_EOF
 [Unit]
@@ -23,6 +27,8 @@ SVC_EOF
 systemctl daemon-reload
 systemctl enable pgd
 }
+
+create_service
 export dbuser=enterprisedb
 export dbport=5444
 export dbname=edb
@@ -31,7 +37,7 @@ export PG_FLAVOR=edb-as
 export PG_VERSION=17
 export PG_BINDIR=/usr/lib/$PG_FLAVOR/$PG_VERSION/bin
 export PATH=$PATH:$PG_BINDIR
-export PGDATA=/var/lib/$PG_FLAVOR/$PG_VERSION/main/
+export PGDATA=/var/lib/$PG_FLAVOR/$PG_VERSION/pgd
 export PGPASSWORD=secret
 
 hostname=$(hostname)
@@ -48,10 +54,13 @@ db1_dsn="host=db-1 user=$dbuser port=$dbport dbname=$dbname"
 db2_dsn="host=db-2 user=$dbuser port=$dbport dbname=$dbname"
 db3_dsn="host=db-3 user=$dbuser port=$dbport dbname=$dbname"
 
-extra_options="--bindir $PG_BINDIR -D $PGDATA --listen-addr '*'"
+extra_options="--bindir $PG_BINDIR -D $PGDATA"
 
 case $hostname in
   db-1)
+    echo "Create the initial database"
+    ${PG_BINDIR}/initdb -U enterprisedb ${PGDATA}
+
     echo "execute: pgd node node-1 setup --dsn \"$db1_dsn\"  --group-name group-1 $extra_options"
     pgd node node-1 setup --dsn "$db1_dsn"  --group-name group-1 $extra_options
     ;;
@@ -73,6 +82,4 @@ psql -p $dbport -U $dbuser postgres -c "ALTER USER enterprisedb PASSWORD '$PGPAS
 psql -p $dbport -U $dbuser postgres -c "ALTER SYSTEM SET listen_addresses = '*';"
 EOF
 
-create_service
-systemctl restart pgd # Ensure clean start
-until $PG_BINDIR/pg_isready -h localhost -p $dbport; do sleep 1; done
+restart_db
